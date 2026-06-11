28b3375c5bc089aa3df41f2c2f1a18eee6836d6f
unknown <juan.rodriguez-ce@labinfo.is.escuelaing.edu.co>
2026-06-11 12:30:16 -0500

    Primeros Pasos2

---
+++ movie.grpc/src/test/java/edu/eci/arsw/movie/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
